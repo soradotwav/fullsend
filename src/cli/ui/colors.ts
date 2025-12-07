05f6d7c62e9b5a1fa8174cdee1e6870fe7b50c4c
soradotwav <dev@soradotwav.com>
2025-12-07 00:16:32 -0800

    feat: add CLI color utility and chalk dependency

---
+++ src/cli/ui/colors.ts
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+
+export const colors = {
+  dim: chalk.rgb(161, 161, 170),
+  dimmer: chalk.rgb(82, 82, 91),
+  success: chalk.rgb(34, 197, 94),
+  error: chalk.rgb(239, 68, 68),
+  warning: chalk.rgb(234, 179, 8),
+  text: chalk.rgb(250, 250, 250),
+  accent: chalk.rgb(56, 189, 248),
+};
